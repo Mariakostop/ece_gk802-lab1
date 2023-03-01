cbdd87cb3dc60a2822aeca465debf2476d456617
Μαρια Κωστοπουλου <up1072894@upnet.gr>
2023-03-01 21:49:45 +0200

    cookies

--- lab1.py
+++ lab1.py
@@ -1,7 +1,22 @@
 import requests
+from datetime import datetime
 
-i = input("give an url: ")
+i = input("Give url: ")
 r = requests.get(i)
 for j in r.headers :
     print(j)
     
+print("software: "+r.headers["Server"])
+
+if "Set-Cookie" in r.headers :
+    for c in r.cookies :
+        print("cookie_name: "+c.name)
+        if c.expires==None:
+            print("Expire date doesn't exist")
+        else:
+            exp_date=c.expires
+            now=datetime.now().timestamp()
+            print("Cookie valid for:"+str(exp_date-now))
+            
+    
+else : print("No cookies")
